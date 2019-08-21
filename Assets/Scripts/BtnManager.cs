b55935fc55623b1cb5f28bbd8f7c407cbb7acdb9
roynbo <roynbo@sjtu.edu.cn>
2019-08-21 21:41:48 +0800

    8-22-mission
    8-22任务，UI界面设计

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BtnManager : MonoBehaviour
+{
+    IOManager leftIO, rightIO;
+    [SerializeField] Toggle[] leftToggles = new Toggle[2];
+    [SerializeField] Toggle[] rightToggles = new Toggle[4];
+    [SerializeField] float sleepTime = 3.0f;
+    float leftOnTime, rightOnTime;
+    float waitTime = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftIO = new IOManager(2);
+        rightIO = new IOManager(4);
+        leftIO.ToggleSet = leftToggles;
+        rightIO.ToggleSet = rightToggles;
+        leftIO.Init();
+        rightIO.Init();
+        leftIO.sleepTimeSet = sleepTime;
+        rightIO.sleepTimeSet = sleepTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftIO.Monitor();
+        rightIO.Monitor();
+    }
+}
---
+++ Assets/Scripts/IOManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IOManager
+{
+    private int m_IO_num;
+    private Toggle[] m_toggles;
+    private float m_on_time;
+    private float m_sleep_time;
+    private Toggle m_chosen_toggle;
+    private bool m_can_chose;
+    public IOManager(int num)
+    {
+        m_IO_num = num;
+        m_toggles = new Toggle[m_IO_num];
+        m_on_time = 0;
+        m_sleep_time = 3.0f;
+        m_can_chose = true;
+    }
+    public Toggle[] ToggleSet
+    {
+        set
+        {
+            m_toggles = value;
+        }
+    }
+    public float sleepTimeSet
+    {
+        set
+        {
+            m_sleep_time = value;
+        }
+    }
+    public void Init()
+    {
+        foreach (Toggle toggle in m_toggles)
+        {
+            toggle.isOn = false;
+        }
+    }
+    public void Monitor()
+    {
+        for(int i=0;i<m_IO_num;i++)
+        {
+            if (m_can_chose)
+            {
+                if (m_toggles[i].isOn)
+                {
+                    m_chosen_toggle = m_toggles[i];
+                    m_on_time = Time.time;
+                    m_can_chose = false;
+                }
+            }
+            else
+            {
+                m_toggles[i].interactable = false;
+            }
+        }
+        if(m_on_time+m_sleep_time<Time.time)
+        {
+            if (m_chosen_toggle != null)
+            {
+                m_chosen_toggle.isOn = false;
+                m_can_chose = true;
+                for(int i=0;i<m_IO_num;i++)
+                {
+                    m_toggles[i].interactable = true;
+                }
+            }
+        }
+    }
+}
